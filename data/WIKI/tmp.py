a66abdfdb42b19ed2814f3d8c355fe28d4644336
xyj <bigbangxie@gmail.com>
2024-05-08 11:33:45 +0800

    model files

---
+++ data/WIKI/tmp.py
@@ -0,0 +1,11 @@
+import random
+
+def f(in_path, o_path):
+    o_file = open(o_path, 'a')
+    with open(in_path) as file:
+        for i, _ in enumerate(file):
+            txt =  1 if random.random() < 0.8 else 0 
+            o_file.write(str(txt) + '\n')
+    o_file.close()
+
+f('models/2/CENET-Part2-TKGI/data/WIKI/train.txt', 'models/2/CENET-Part2-TKGI/data/WIKI/train-llm.txt')
